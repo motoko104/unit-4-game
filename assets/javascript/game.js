9784a2e0a41b1ec1584ff5264b3610ad09a144ce
Manda104 <motoko104@yahoo.com>
2018-08-19 23:33:09 -0500

    added image and started javascript object creations

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    
+    let goodCharacters = {
+        'Obi Wan': {
+            name: 'Obi Wan',
+            hP: 200,
+            attack: 15,
+            imageUrl:
+        },
+        'Aayla Secura':{
+            name: 'Aayla Secura',
+            hP: 150,
+            attack: 10,
+            imageUrl:
+        },
+        'Samuel Jackson':{
+            name: 'Samuel Jackson',
+            hP: 225,
+            attack: 15,
+            imageUrl: 
+        }   
+    }
+    let badCharacters = {
+        'Darth Maul':{
+            name:'Darth Maul',
+            hP: 175,
+            attack: 10,
+            imageUrl: 
+        },
+        'General Greivus':{
+            name: 'General Greivus',
+            hP: 200,
+            attack: 15,
+            imageUrl:
+        },
+        'Darth Sidious':{
+            name: 'Darth Sidious',
+            hP: 250,
+            attack: 20,
+            imagUrl:
+        }
+    }
+
+});
